e83c70b6fe93382bc87af50780379430aea8d716
Joseph Songer <jcs4te@labmini00-2.local>
2015-10-20 13:25:18 -0400

    Initial Commit

---
+++ TripTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TripTracker
+//
+//  Created by Joseph Songer on 10/20/15.
+//  Copyright © 2015 UVa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
