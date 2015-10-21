ae315da1eea81304621303fd43898d5129cecfbf
Joseph Songer <jcs4te@labmini13.lab.cs.virginia.edu>
2015-10-21 13:23:59 -0400

    Finished milestone one, added map/user input

--- TripTracker/ViewController.swift
+++ TripTracker/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var tripNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        //hide keyboard
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        tripNameLabel.text = textField.text
+    }
 
+    // MARK: Actions
 
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        tripNameLabel.text = "Default Text"
+    }
+    
 }
 
